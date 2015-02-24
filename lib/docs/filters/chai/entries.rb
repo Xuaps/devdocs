6172599e74eae12a0f5c40923b4653976c00b0da
Alvaro Verdión <averdion@gmail.com>
2015-02-24 17:47:36 +0100

    added chai

--- lib/docs/filters/chai/entries.rb
+++ lib/docs/filters/chai/entries.rb
@@ -5,16 +5,46 @@ module Docs
         at_css('h1').content
       end
 
+      def get_docset
+        docset = context[:root_title]
+        docset
+      end
+
+      def get_parsed_uri
+        parsed_uri = context[:docset_uri] + '/' + path.sub('/index','')
+        parsed_uri
+      end
+
+      def get_parent_uri
+        subpath = *path.split('/')
+        if subpath.length > 1
+            parent_uri = (context[:docset_uri]+ '/' + subpath[0,subpath.size-1].join('/')).downcase
+        else
+            parent_uri = 'null'
+        end
+      end
+
       def get_type
-        subpath.start_with?('/guide') ? 'Guides' : nil
+        if path.include? 'assert'
+            'Assert'
+        elsif path.include? 'bdd'
+             'Bdd'
+        elsif path.include? 'plugin'
+             'Plugin'
+        elsif path.include? 'installation'
+             'Installation'
+        elsif path.include? 'bdd'
+             'Bdd'
+        else
+             'others'
+        end
       end
 
       def additional_entries
         css('.antiscroll-inner a').each_with_object [] do |node, entries|
           id = node['href'].remove('#') + '-section'
-          node.content.strip.split(' / ').uniq { |name| name.downcase }.each do |name|
-            entries << [name, id, self.name]
-          end
+          name = node.content.strip.split(' / ')[0]
+          entries << [name, id, type, get_parsed_uri, get_parent_uri, get_docset]
         end
       end
     end
--- lib/docs/scrapers/chai.rb
+++ lib/docs/scrapers/chai.rb
@@ -5,14 +5,16 @@ module Docs
     self.version = '1.10.0'
     self.base_url = 'http://chaijs.com'
     self.root_path = '/api/'
-    self.initial_paths = %w(/guide/installation/)
+    self.initial_paths = %w(/guide/installation/ /api/bdd/)
 
     html_filters.push 'chai/entries', 'chai/clean_html'
 
     options[:container] = '#content'
     options[:trailing_slash] = true
+    options[:root_title] = 'Chai'
+    options[:docset_uri] = '/chai'
 
-    options[:only_patterns] = [/\A\/guide/, /\A\/api/]
+    options[:only_patterns] = [/\A\/guide/, /\A\/api*/]
     options[:skip] = %w(/api/test/ /guide/ /guide/resources/)
 
     options[:attribution] = <<-HTML
