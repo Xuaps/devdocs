d0a905c469e99ad3e9af95c9753603f6c3168424
Alvaro Verdión <averdion@gmail.com>
2015-04-21 21:28:30 +0200

    c++ breadcrumbs  version

--- lib/docs/filters/cpp/entries.rb
+++ lib/docs/filters/cpp/entries.rb
@@ -1,23 +1,28 @@
 module Docs
   class Cpp
     class EntriesFilter < Docs::EntriesFilter
-      EXCLUDED_PATH = ['C++']
+      EXCLUDED_PATH = ['C++', 'Standard Library header files', 'std::vector<bool>']
 
       REPLACE_NAMES = {
-        'Error directive' => '#error directive',
-        'Filename and line information' => '#line directive',
-        'Implementation defined behavior control' => '#pragma directive',
-        'Replacing text macros' => '#define directive',
+        'Concepts' => 'C++ concepts',
+        'Utility library' => 'Utilities library',
+        'C Date and time utilities' => 'C-style date and time utilities',
+        'Localization library' => 'Localizations library',
+        'Experimental C++ standard libraries' => 'Experimental libraries',
         'Source file inclusion' => '#include directive' }
 
+      REPLACE_PARENTS = {
+        '/cpp/dynamic_memory_management' => '/cpp/utilities_library/dynamic_memory_management'}
+
       def get_name
-        name = at_css('#firstHeading').content.strip
-        name.remove! 'C++ concepts: '
-        name.remove! 'C++ keywords: '
-        name.remove! 'C++ '
+        if at_css('.t-navbar-head >.selflink')
+            name = at_css('.t-navbar-head >.selflink').content.strip
+        else
+            name = at_css('#firstHeading').content.strip
+        end
+        name.remove! 'std::experimental::filesystem::'
         name.remove! %r{\s\(.+\)}
         name.sub! %r{\AStandard library header <(.+)>\z}, '\1'
-        name = name.split(',').first
         name = REPLACE_NAMES[name] || name
         if slug.end_with? '2'
             name += '2'
@@ -26,9 +31,6 @@ module Docs
       end
 
       def get_custom_name(customname)
-        customname.remove! 'C++ concepts: '
-        customname.remove! 'C++ keywords: '
-        customname.remove! 'C++ '
         customname.remove! %r{\s\(.+\)}
         customname.sub! %r{\AStandard library header <(.+)>\z}, '\1'
         customname = REPLACE_NAMES[customname] || customname
@@ -39,10 +41,6 @@ module Docs
       end
 
       def get_parsed_uri_by_name(customname)
-        puts 'subname: ' + customname
-        customname.remove! 'C++ concepts: '
-        customname.remove! 'C++ keywords: '
-        customname.remove! 'C++ '
         customname.remove! %r{\s\(.+\)}
         customname.sub! %r{\AStandard library header <(.+)>\z}, '\1'
         customname = REPLACE_NAMES[customname] || customname
@@ -50,8 +48,6 @@ module Docs
             customname += '2'
         end
         customparsed_uri = get_parsed_uri + '/' + self.urilized(customname)
-        puts 'elseparsed_uri: ' + customparsed_uri
-        customparsed_uri
       end
       
       def get_docset
@@ -70,45 +66,55 @@ module Docs
       end
 
       def get_parent_uri
-        parent_uri = 'null'
-        # parent_uri = context[:docset_uri]
-        # xpath('//div[@class="t-navbar-head"]/a').each do |node|
-        #    link = node.content.strip
-        #    if not EXCLUDED_PATH.include? link
-        #        parent_uri += '/' + self.urilized(link)
-        #    end
-        # end
+        parent_uri = context[:docset_uri]
+        if xpath('//*[@class="t-navbar"]').size >= 2
+            xpath('//*[@class="t-navbar"]').first.remove
+        end
+        xpath('//div[@class="t-navbar-head"]/a').each do |node|
+           link = node.content.strip
+           if not EXCLUDED_PATH.include? link and link != get_name
+              parent_uri += '/' + self.urilized(link)
+           end
+        end
+        if parent_uri == context[:docset_uri]
+            parent_uri = 'null'
+        end
+        REPLACE_PARENTS.each do  |key, value|
+           parent_uri.sub! key, value
+        end
         parent_uri
       end
 
       def get_type
-        if at_css('#firstHeading').content.include?('C++ keyword')
-          'Keywords'
-        elsif subpath.start_with?('experimental')
-          'Experimental libraries'
-        elsif type = at_css('.t-navbar > div:nth-child(4) > :first-child').try(:content)
-          type.strip!
-          type.remove! ' library'
-          type.remove! ' utilities'
-          type.remove! 'C++ '
-          type.capitalize!
-          type
+        if slug.include? 'keyword'
+            type = 'language'
+        elsif slug.include? 'container'
+            type = 'operator'
+        elsif slug.include? 'type'
+            type = 'type'
+        elsif slug.include? 'utility'
+            type = 'function'
+        elsif slug.include? 'function'
+            type = 'function'
+        elsif slug.include? 'class'
+            type = 'class'
+        else
+            type = 'others'
         end
+        type
       end
 
       def additional_entries
-         puts 'path: #' + path + '#'
          return [] unless include_default_entry?
          names = at_css('#firstHeading').content.remove(%r{\(.+?\)}).split(',')[1..-1]
          names.each(&:strip!).reject! do |name|
            name.size <= 2 || name == '...' || name =~ /\A[<>]/ || name.start_with?('operator')
          end
-         names.map { |mapname| [get_custom_name(mapname),'additional',get_type,get_parsed_uri_by_name(mapname),get_parsed_uri, get_docset] }
+         names.map { |mapname| [get_custom_name(mapname),nil,get_type,get_parsed_uri_by_name(mapname),get_parsed_uri, get_docset] }
        end
 
        def include_default_entry?
-         @include_default_entry = (at_css('.t-navbar > div:nth-child(4) > a') && at_css('#firstHeading').content !~ /\A\s*operator./) || path = 'index'
-         puts 'included: ' + @include_default_entry.to_s
+         @include_default_entry = true #at_css('.t-navbar > div:nth-child(4) > a') && at_css('#firstHeading').content !~ /\A\s*operator./
          return @include_default_entry
        end
     end
