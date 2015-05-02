3717422f6aa8cdabd60fe9d9d05b3016cb96178e
Alvaro Verdión <averdion@gmail.com>
2015-05-02 14:17:01 +0200

    fixed links in Nginx docset

--- lib/docs/filters/nginx/clean_html.rb
+++ lib/docs/filters/nginx/clean_html.rb
@@ -1,6 +1,9 @@
 module Docs
   class Nginx
     class CleanHtmlFilter < Filter
+      BROKEN_LINKS = [
+
+      ]
       def call
         at_css('h2').name = 'h1'
 
@@ -22,7 +25,37 @@ module Docs
           at_css('h1 + table').replace("<ul><li>#{links.join('</li><li>')}</li></ul>")
         end
 
+        css('a[href]').each do |node|
+          node['href'] = CleanWrongCharacters(node['href'])
+          if !node['href'].start_with? 'http://' and !node['href'].start_with? 'https://'
+            if BROKEN_LINKS.include?node['href'].downcase
+              node['class'] = 'broken'
+              node['href'] = '/help#brokenlink'
+            else
+              sluglist = slug.split('/')
+              nodelist = node['href'].split('/')
+              newhref = []
+              nodelist.each do |item|
+                if item == '..'
+                  sluglist.pop
+                else
+                  newhref << item
+                end
+              end
+              sluglist.pop
+              if sluglist.size>0
+                node['href'] = sluglist.join('/') + '/' + newhref.join('/')
+              else
+                node['href'] = newhref.join('/')
+              end
+            end
+          end
+        end
+
         doc
+      end
+      def CleanWrongCharacters(href)
+          href.gsub('%23', '#').gsub('%28', '(').gsub('%29', ')').gsub('%21', '!').gsub('%7b', '{').gsub('%7e', '~').gsub('%2a', '*').gsub('%2b', '+').gsub('%3d', '=').gsub('%40', '@')
       end
     end
   end
