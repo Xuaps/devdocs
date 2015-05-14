537e8df0a098fbe3d7a559f4ee20d720e2b7dfcf
Alvaro Verdión <averdion@gmail.com>
2015-05-14 19:47:24 +0200

    added Nokogiri docset

--- lib/docs/filters/nokogiri2/entries.rb
+++ lib/docs/filters/nokogiri2/entries.rb
@@ -1,10 +1,78 @@
 module Docs
   class Nokogiri2
-    class EntriesFilter < Docs::Rdoc::EntriesFilter
+    class EntriesFilter < Docs::EntriesFilter
+      EXCLUDED_PATH = ['Libraries']
+      def get_name
+        if at_css('h1')
+          name = at_css('h1').content.strip
+          if name.index('::')
+            name = name.split('::').last
+          end
+        else
+          name = slug
+        end
+        name.remove! 'Module: '
+        name.remove! 'Class: '
+        name
+      end
+
+      def get_docset
+        docset = context[:root_title]
+        docset
+      end
+
+      def get_parsed_uri_by_name(name)
+        parsed_uri = context[:docset_uri] + '/' + self.urilized(name)
+        parsed_uri
+      end
+
+      def get_parsed_uri
+        if get_parent_uri == 'null'
+            parsed_uri = context[:docset_uri] + '/' + self.urilized(get_name)
+        else
+            parsed_uri = get_parent_uri + '/' + self.urilized(get_name)
+        end
+        parsed_uri
+      end
+
+      def get_parent_uri
+        parent_uri = context[:docset_uri]
+        xpath('//div[@id="menu"]//a/text()').each do |node|
+           link = node.content.strip
+           if not EXCLUDED_PATH.include? link and !link.start_with? 'Index'
+              parent_uri += '/' + self.urilized(link)
+           end
+        end
+        if parent_uri == context[:docset_uri]
+            parent_uri = 'null'
+        end
+        parent_uri
+      end
+
       def get_type
-        type = name.dup
-        type.remove! %r{#.+\z}
-        type.split('::')[0..2].join('::')
+        if at_css('h1')
+          name = at_css('h1').content.strip
+        else
+          name = 'others'
+        end
+        if name.start_with? 'Module'
+          type = 'module'
+        elsif name.start_with? 'Class'
+          type = 'class'
+        else
+          type = 'others'
+        end
+        type
+      end
+
+      def additional_entries
+        entries = []
+        css('h3.signature').each do |node|
+          name = node.content.strip
+          custom_parsed_uri = get_parsed_uri_by_name(name)
+          entries << [name, node['id'], 'method', custom_parsed_uri, get_parsed_uri, get_docset]
+        end
+        entries
       end
     end
   end
--- lib/docs/scrapers/nokogiri2.rb
+++ lib/docs/scrapers/nokogiri2.rb
@@ -1,17 +1,19 @@
 module Docs
-  class Nokogiri2 < Rdoc
+  class Nokogiri2 < UrlScraper
     self.name = 'Nokogiri'
     self.slug = 'nokogiri'
     self.version = '1.6.4'
-    self.dir = '/Users/Thibaut/DevDocs/Docs/RDoc/Nokogiri'
-
-    html_filters.replace 'rdoc/entries', 'nokogiri2/entries'
+    self.base_url =  'http://www.rubydoc.info/gems/nokogiri/1.6.4/'
+    self.root_path = 'index'
+    self.initial_paths = %w(
+      toplevel
+    )
+    html_filters.push 'nokogiri2/entries', 'nokogiri2/clean_html'
 
     options[:domain] = 'http://www.refly.co'
     options[:root_title] = 'Nokogiri'
-    options[:root_title] = 'Nokogiri'
-    options[:docset_uri] = '/nokogiri' 
-    options[:only_patterns] = [/\ANokogiri/]
+    options[:docset_uri] = '/nokogiri'
+    options[:container] = '#content'
 
     options[:attribution] = <<-HTML
       &copy; 2008&ndash;2014 Aaron Patterson, Mike Dalessio, Charles Nutter,<br>
