3562fa125d056c31c35a5e44aaa5df5237bf69f2
Alvaro Verdión <averdion@gmail.com>
2015-05-27 08:44:19 +0200

    added function for formating pre elements

--- lib/docs/core/filter.rb
+++ lib/docs/core/filter.rb
@@ -78,14 +78,29 @@ module Docs
         href.gsub('%23', '#').gsub('%40', '@').gsub('%28', '(').gsub('%29', ')').gsub('%21', '!').gsub('%7b', '{').gsub('%7e', '~').gsub('%2a', '*').gsub('%2b', '+').gsub('%3d', '=')
     end
 
-    def WrapContentWithDivs(classname,doc)
-        divremovable = Nokogiri::XML::Node.new "div", doc
-        divcontainer = Nokogiri::XML::Node.new "div", doc
+    def remove_html_tags(content)
+      re = /<("[^"]*"|'[^']*'|[^'">])*>/
+      return content.gsub!(re, '')
+    end
+
+    def WrapPreContentWithCode(classname)
+      css('pre').each do |node|
+        node['class'] = classname
+        if !node.children[0] || node.children[0].name != 'code'
+            codetag = Nokogiri::XML::Node.new "code", @doc
+            codetag.children = node.children
+            node.children = codetag
+        end
+      end
+    end
+
+    def WrapContentWithDivs(classname)
+        divremovable = Nokogiri::XML::Node.new "div", @doc
+        divcontainer = Nokogiri::XML::Node.new "div", @doc
         divcontainer['class'] = classname
-        divcontainer.children = doc
+        divcontainer.children = @doc
         divremovable.children = divcontainer
-        doc = divremovable
-        doc
+        @doc = divremovable
     end
   end
 end
