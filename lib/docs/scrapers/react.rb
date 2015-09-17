a7c3af8741de4c53c7bdf870e91fbb87b8a5b4e6
Alvaro Verdión <averdion@gmail.com>
2015-09-17 08:47:55 +0200

    updated react scrapper

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -2,20 +2,23 @@ module Docs
   class React < UrlScraper
     self.name = 'React'
     self.type = 'react'
-    self.version = '0.12.2'
-    self.base_url = 'http://facebook.github.io/react/docs/'
-    self.root_path = 'getting-started.html'
+    self.version = '0.13.3'
+    self.base_url = 'http://facebook.github.io/react/'
+    self.root_path = 'docs/getting-started.html'
     html_filters.push 'react/entries', 'react/clean_html'
 
     options[:domain] = 'http://www.refly.xyz'
     options[:container] = '.documentationContent'
     options[:root_title] = 'React'
     options[:docset_uri] = '/react'
-
+    options[:only_patterns] = [/\Adocs\//, /\Atips\//]
     options[:skip] = %w(
-      videos.html
-      complementary-tools.html
-      examples.html)
+      docs/
+      docs/videos.html
+      docs/complementary-tools.html
+      docs/examples.html
+      docs/conferences.html
+      tips/introduction.html)
 
     options[:attribution] = <<-HTML
       &copy; 2013&ndash;2014 Facebook Inc.<br>
--- scrapper.sh
+++ scrapper.sh
@@ -21,7 +21,7 @@ then
 	date=`/bin/date "+%d/%m/%Y -%H:%M:%S"`
 	echo "scrapping started at $date"
 	OUTPUTLOG='errors.log'
-	for docset in angular backbone bower c chai coffeescript cordova cpp css d3 django dom dom_events ember express git go grunt html http haskell jquery_core jquery_ui jquery_mobile javascript knockout laravel less lodash marionette markdown modernizr moment mongoose nginx node nokogiri2 npm php2 phpunit postgresql python2 python rails react redis requirejs rethinkdb ruby sass sinon socketio svg underscore xpath yii
+	for docset in angular apache backbone bower c chai clojure coffeescript cordova cpp css d3 django dom dom_events drupal ember express git go grunt html http haskell jquery_core jquery_ui jquery_mobile javascript knockout laravel less lodash lua marionette markdown mocha modernizr moment mongoose nginx node nokogiri2 npm opentsdb phalcon phaser php2 phpunit postgresql python2 python q rails react redis requirejs rethinkdb ruby rust sass sinon socketio svg symfony underscore vue webpack xpath yii yii1
 	do
 		if thor docs:generate $docset --force
 		then
